f00b0d88b04b88aafbfd25b201464e92f24a2659
WendyKariuki <phaminuswendy@gmail.com>
2024-05-24 16:07:31 +0300

    Anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -3,5 +3,11 @@ class Anagram:
         self.word = word
 
     def match(self, candidates):
-        return [w for w in candidates if self.word != w and sorted(self.word) == sorted(w)]
+        word_sorted = sorted(self.word)
+        matches = []
+        for candidate in candidates:
+            if candidate != self.word and sorted(candidate) == word_sorted:
+                matches.append(candidate)
+        return matches
+     
     
